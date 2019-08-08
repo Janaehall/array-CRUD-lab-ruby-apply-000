09ab7a91244c2558a78066d74b64394db2d03aab
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-08-08 06:27:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Hello", "Hi" "How are you?"]
+  ["Hello", "Hi" "How are you?", "Hey there"]
 end
 
 def add_element_to_end_of_array(array, element)
